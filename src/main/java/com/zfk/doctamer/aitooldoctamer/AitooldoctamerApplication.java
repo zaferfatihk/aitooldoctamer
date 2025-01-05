147974c56ef9b02fcca4e67b538bf768f677fcf5
Zafer Fatih Koyuncu <zaferfatih@gmail.com>
2025-01-05 18:58:57 +0300

    hello world

--- src/main/java/com/zfk/doctamer/aitooldoctamer/AitooldoctamerApplication.java
+++ src/main/java/com/zfk/doctamer/aitooldoctamer/AitooldoctamerApplication.java
@@ -2,12 +2,20 @@ package com.zfk.doctamer.aitooldoctamer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class AitooldoctamerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AitooldoctamerApplication.class, args);
 	}
+	
+	@RequestMapping("/")
+    String home() {
+        return "Helloooooo World!";
+    }
 
 }
